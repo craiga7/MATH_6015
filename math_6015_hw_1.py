604a5414d28a35f0c1126aa71a9fcd4324610fe2
craiga7 <craiga7@mail.uc.edu>
2024-01-12 13:09:40 -0500

    git commit

--- math_6015_hw_1.py
+++ math_6015_hw_1.py
@@ -5,5 +5,17 @@ AFFILIATION: University of Cincinnati
 DATE: 17JAN24
 ASSIGNMENT: HW 1
 """
-a = 1+1
-print(a)
+
+# Prompt user for their name
+name = input("Enter your first name: ")
+
+# Initialize a variable for storing the results
+value = 0
+
+# Loop through each character in the name, 
+# converting each to its ASCII value, and add to the running value
+for c in name:
+    value += ord(c) # ord(C) obtains the ASCII value of character
+    
+# Print the resulting value
+print(name, " becomes ", value)
---
+++ math_6015_lect_1.py
@@ -0,0 +1,27 @@
+"""
+Class: MATH6015
+BY: AARON CRAIG
+"""
+import os
+import sys
+
+## VARIABLES
+m = 2
+x = 2.0
+z = 2.0 + 0j
+
+print('Types of Variable Relationships')
+print(type(m))
+print(type(x))
+print(type(z))
+print(type(m+x))
+print(type(x+z))
+print(type(m/m))
+print(type(m//m))
+
+# Truth Testing/Boolean Variables
+print('Truth Testing:')
+# z = 2.0
+print(z is x)
+
+
