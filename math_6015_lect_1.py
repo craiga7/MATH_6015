53c53d55e934e507950a2dc790cf36f014ab81a3
craiga7 <craiga7@mail.uc.edu>
2024-01-16 13:41:00 -0500

    git commit

--- math_6015_lect_1.py
+++ math_6015_lect_1.py
@@ -24,4 +24,17 @@ print('Truth Testing:')
 # z = 2.0
 print(z is x)
 
+# Special values
+a = 0.1
+b = a + a
+c = a + b
+
+print('Special Values')
+print(a)
+print(b)
+print(c)
+
+print((0.1+0.2)==0.3)
+print((1+2)/10 == 0.3)
+
 
